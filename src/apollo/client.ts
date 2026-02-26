1c3404f119e22ba2a7c01f0eb6e64fc910fc175a
Mark Larah <mark@larah.me>
2026-02-26 14:46:50 -0600

    simplify

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -15,35 +15,13 @@ const mockLink = new MockLink({
 // Compose links
 const link = from([mockLink, httpLink]);
 
-// Create Apollo Client with relaxed type policies to allow mocking fields that don't exist
+// Create Apollo Client
 export const client = new ApolloClient({
   link,
-  cache: new InMemoryCache({
-    typePolicies: {
-      Country: {
-        fields: {
-          // Allow any field on Country type (for mocking non-existent fields)
-          population: {
-            read(existing) {
-              return existing;
-            },
-          },
-          weather: {
-            read(existing) {
-              return existing;
-            },
-          },
-        },
-      },
-    },
-  }),
+  cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
       fetchPolicy: "cache-and-network",
-      errorPolicy: "all", // Don't throw errors, return them in the result
-    },
-    query: {
-      errorPolicy: "all",
     },
   },
 });
